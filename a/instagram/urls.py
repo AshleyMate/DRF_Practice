db9ddc16ec4a2cda33d2a1eb8dc0f4a500c461f7
chanyoung <young9484825@gmail.com>
2020-08-04 15:56:23 +0900

    APIView를 활용한 뷰

--- a/instagram/urls.py
+++ a/instagram/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('post', views.PostViewSet)
+router.register('post', views.PostViewSet)  # 2개 URL을 만들어줍니다.
+# router.urls  # url pattern list
+
 urlpatterns = [
-    path('public/', views.PublicPostListAPIView.as_view()),
+    path('public/', views.public_post_list),
     path('', include(router.urls)),
 ]
